58f53ea556d71777969d1dfab9e09b0bcfba1b25
Lucas Murakami <lkenjim98@gmail.com>
2021-02-02 16:23:22 -0300

    Throwing custom error if user has not been found

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,22 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
+const NotFoundError = require('../errors/NotFoundError');
 
 class UserController {
-	
+	async createSession() {
+		const user = await this._findByEmail(email);
+		if (!user) throw new NotFoundError('User not found');
+	}
+
+	_findByEmail(email) {
+		return User.findOne({
+			where: { email },
+			attributes: {
+				exclude: ['createdAt', 'updatedAt']
+			}
+		});
+	}
 }
 
 module.exports = new UserController();
