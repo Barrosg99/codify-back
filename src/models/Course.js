d3dc4a52aba734b4a000a855932340de1dfcf6c5
hoffmannpaola <hoffmannpaola@gmail.com>
2021-02-02 17:39:59 -0300

    Criated Course model

---
+++ src/models/Course.js
@@ -0,0 +1,33 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../utils/database');
+
+class Course extends Sequelize.Model {}
+
+Course.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'course',
+  },
+);
+
+module.exports = Course;
