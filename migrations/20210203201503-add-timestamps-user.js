d4039343d90930a11d8a6a488bf2f1ceee32a07a
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-03 17:18:23 -0300

    add timestamps at users table

---
+++ migrations/20210203201503-add-timestamps-user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.addColumn('users',
+			'createdAt',
+			{
+				type: Sequelize.DATE,
+			});
+		await queryInterface.addColumn('users',
+			'updatedAt',
+			{
+				type: Sequelize.DATE,
+			});
+	},
+
+	down: async (queryInterface) => {
+		await queryInterface.removeColumn('users', 'createdAt');
+		await queryInterface.removeColumn('users', 'updatedAt');
+	}
+};
