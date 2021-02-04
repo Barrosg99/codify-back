c356190322820743d48a99c498fac8e0d14eb54b
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-04 16:32:13 -0300

    adjust

--- migrations/20210203201503-add-timestamps-user.js
+++ migrations/20210203201503-add-timestamps-user.js
@@ -1,21 +1,19 @@
-'use strict';
-
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		await queryInterface.addColumn('users',
-			'createdAt',
-			{
-				type: Sequelize.DATE,
-			});
-		await queryInterface.addColumn('users',
-			'updatedAt',
-			{
-				type: Sequelize.DATE,
-			});
-	},
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('users',
+      'createdAt',
+      {
+        type: Sequelize.DATE,
+      });
+    await queryInterface.addColumn('users',
+      'updatedAt',
+      {
+        type: Sequelize.DATE,
+      });
+  },
 
-	down: async (queryInterface) => {
-		await queryInterface.removeColumn('users', 'createdAt');
-		await queryInterface.removeColumn('users', 'updatedAt');
-	}
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('users', 'createdAt');
+    await queryInterface.removeColumn('users', 'updatedAt');
+  },
 };
