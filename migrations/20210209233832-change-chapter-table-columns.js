eef3e5f626a1d02bae555ea8475f53563833c5fd
Lucas Murakami <lkenjim98@gmail.com>
2021-02-09 20:42:33 -0300

    Chapter model / 'chapters' table columns bug fix

---
+++ migrations/20210209233832-change-chapter-table-columns.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('chapters', 'topicsQuantity', {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    });
+
+    await queryInterface.changeColumn('chapters', 'exercisesQuantity', {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('chapters', 'topicsQuantity', {
+      type: Sequelize.INTEGER
+    });
+
+    await queryInterface.changeColumn('chapters', 'exercisesQuantity', {
+      type: Sequelize.INTEGER
+    });
+  }
+};
